805cbd0c3c73edb48d5b62a4c26581024003d314
CKanishka <30779692+CKanishka@users.noreply.github.com>
2017-08-08 01:48:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,91 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articles={
+    'article-One':{
+        title:'ARTICLE ONE|KANISHKA CHOWDHURY',
+        heading:'ARTICLE ONE',
+        content:`
+        <p>
+                This is the content for my first article. 
+            </p>
+            <p>
+                This is the content for my first article. 
+            </p>
+            <p>
+                This is the content for my first article. 
+            </p> `
+    },
+        'article-Two':{
+        title:'ARTICLE TWO|KANISHKA CHOWDHURY',
+        heading:'ARTICLE TWO',
+        content:`
+        <p>
+                This is the content for my second article. 
+            </p>
+            <p>
+                This is the content for my second article. 
+            </p>
+            <p>
+                This is the content for my second article. 
+            </p> `
+    },
+
+    'article-Three':{
+        title:'ARTICLE THREE|KANISHKA CHOWDHURY',
+        heading:'ARTICLE THREE',
+        content:`
+        <p>
+                This is the content for my third article. 
+            </p>
+            <p>
+                This is the content for my third article. 
+            </p>
+            <p>
+                This is the content for my third article. 
+            </p> `
+    },
+
+};
+
+function createTemplate(data){
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+    
+    var htmlTemplate= `
+    <html>
+    <head>
+        <title>
+           $(title)
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div Class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3>
+            $(heading)
+        </h3>
+        <div>
+            $(date)
+        </div>
+        <div>
+            $(content)
+        </div>
+        </div>
+    </body>
+</html>
+`;
+return htmlTemplate;
+
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -18,17 +103,13 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 
-app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/:articleName', function (req, res) {
+    var articleName=req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
 
-app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
+
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
